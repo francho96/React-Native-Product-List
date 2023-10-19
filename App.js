64739ddcf2e25ed402e43ab5a7f2423a9f6260ae
francho96 <franco.barria@usach.cl>
2023-10-18 23:13:49 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,231 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+  SectionList,
+  Alert,
+} from 'react-native';
+import { useState } from 'react';
+import { phones, tablets, books, pc } from './data';
+
+const sections = [
+  {
+    title: 'Celulares',
+    data: phones,
+  },
+  {
+    title: 'Tablets',
+    data: tablets,
+  },
+  {
+    title: 'PC',
+    data: pc,
+  },
+  {
+    title: 'Libros',
+    data: books,
+  },
+]
+
+const allSections = sections.map(section => section.title);
+const allProducts = sections.reduce((products, section) => {
+  return products.concat(section.data);
+}, []);
 
 export default function App() {
+
+  const [filter, setFilter] = useState('');
+  const [chipActivated, setChipActivated] = useState(false);
+  const [sectionActivated, setSectionActivated] = useState('');
+
+  function filterByName(arr, searchString) {
+    return arr.filter(function(obj) {
+      return obj.name.toLowerCase().includes(searchString.toLowerCase());
+    });
+  }
+  
+  function productsList(){
+    if (filter === ''){
+      if (chipActivated) {
+        const aux = sections.find(section => section.title.includes(sectionActivated));
+        return [ aux ];
+      }
+      return sections;
+    } else {
+      const auxObject = [
+        {
+          title: 'Resultado:',
+          data: filterByName(allProducts, filter),
+        }
+      ];
+      return auxObject;
+    }
+  }
+
+  function activate(section){
+    if (chipActivated && sectionActivated === section){
+      setChipActivated(false);
+      setSectionActivated('');
+    } else {
+      setChipActivated(true);
+      setSectionActivated(section);
+    }
+  }
+
+  const addToCart = (product) => {
+    Alert.alert('Añadido al carro', `Se ha añadido ${product} al carro`, [
+      {text: 'OK', onPress: () => ''},
+    ]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={setFilter}
+          value={filter}
+          placeholder="Buscar un producto"
+          keyboardType="text"
+          maxLength={20}
+        />
+        {filter === '' && (
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={true}
+            contentContainerStyle={styles.scrollViewContent}
+          >
+            {allSections.map((section) => (
+              <TouchableOpacity
+                style={
+                  sectionActivated === section
+                    ? [styles.chip, styles.chipActivated]
+                    : styles.chip
+                }
+                onPress={() => activate(section)}
+              >
+                <Text style={{ fontSize: 13, fontWeight: 'bold', color: '#333333' }}>{section}</Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        )}
+      </View>
+      <SafeAreaView style={styles.safeViewContainer}>
+        <SectionList
+          sections={productsList()}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item, index }) => {
+              return (
+                <>
+                  <View style={styles.productStyle} key={index}>
+                    <View style={styles.imageArea}>
+                      <Image source={{ uri: item.image }} resizeMode="cover" style={styles.productImage} />
+                    </View>
+                    <View style={styles.descriptionArea}>
+                      <Text style={styles.text}>{item.name}</Text>
+                      <Text>{item.description}</Text>
+                      <Text>{item.price}</Text>
+                    </View>
+                    <View style={styles.buttonArea}>
+                      <TouchableOpacity style={styles.button} onPress={() => addToCart(item.name)} >
+                        <Text>+</Text>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </>
+              );
+          }}
+          renderSectionHeader={({section: {title}}) => (
+            <View style={{ flex: 1, padding: 10, backgroundColor: '#fafafa' }}>
+              <Text style={styles.header}>{title}</Text>
+            </View>
+          )}
+        />
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  safeViewContainer: {
+    flex: 7,
+    paddingTop: StatusBar.currentHeight,
+  },
+  text: {
+    color: '#000000',
+    fontSize: 14,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 60,
+  },
+  productStyle: {
+    flex: 0.3,
+    flexDirection: 'row',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+  productImage: {
+    flex: 1,
+  },
+  imageArea: {
+    flex: 0.7,
+    height: '100%',
+    width: '33%',
+    maxHeight: 50,
+    maxWidth: 50,
+  },
+  descriptionArea: {
+    flex: 2,
+    paddingHorizontal: 10,
+  },
+  buttonArea: {
+    flex: 0.5,
+    width: '17%',
+    justifyContent: 'center',
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    borderRadius: 100,
+    backgroundColor: '#8CB4FF'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    width: '90%',
+  },
+  header: {
+    fontSize: 20,
+  },
+  chip: {
+    display: 'flex',
+    backgroundColor: '#d3d3d3',
+    height: 20,
+    width: '20%',
+    margin: 5,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  chipActivated: {
+    backgroundColor: '#8CB4FF',
+  },
+  scrollViewContent: {
+    flexDirection: 'row',
+    width: '100%',
   },
 });
---
+++ data.js
@@ -0,0 +1,96 @@
+
+export const tablets = [
+    {
+      id: '0',
+      name: 'Tablet X5',
+      price: '$300',
+      description: '4GB RAM + 120GB ROM',
+      image: 'https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/plp/tablets-20230518/matepad-se-series/matepad-se-series-card-1.jpg'
+    },
+    {
+      id: '1',
+      name: 'Tablet X8',
+      price: '$400',
+      description: '6GB RAM + 240GB ROM',
+      image: 'https://http2.mlstatic.com/D_NQ_NP_988706-MLA51345485834_082022-O.webp'
+    },
+    {
+      id: '4',
+      name: 'Tablet YOGA',
+      price: '$400',
+      description: '6GB RAM + 240GB ROM',
+      image: 'https://elchapuzasinformatico.com/wp-content/uploads/2011/07/Panasonic-Toughbook-H2.jpg'
+    },
+    {
+      id: '5',
+      name: 'Tablet iPear',
+      price: '$3000',
+      description: '2GB RAM + 64GB ROM',
+      image: 'https://http2.mlstatic.com/D_NQ_NP_764814-MLA46975173329_082021-O.webp'
+    },
+  ]
+  
+export const phones = [
+    {
+      id: '2',
+      name: 'Celular galactico',
+      price: '$200',
+      description: '4GB RAM + 120GB ROM',
+      image: 'https://f.rpp-noticias.io/2022/04/07/1243876o2xda2m-00jpg.jpg'
+    },
+    {
+      id: '3',
+      name: 'Celular adulto mayor',
+      price: '$20',
+      description: 'teclas XL',
+      image: 'https://altiva.com.mx/wp-content/uploads/2018/07/DJPS7-JPH427_2.jpg'
+    },
+    {
+      id: '6',
+      name: 'Celular free fayer',
+      price: '$50',
+      description: 'sirve para el free',
+      image: 'https://f.rpp-noticias.io/2021/11/24/1178885sin-titulo-3jpg.jpg'
+    },
+    {
+      id: '7',
+      name: 'Celular lujoso',
+      price: '$2000',
+      description: 'teclas de oro',
+      image: 'https://i.blogs.es/a3b121/vertu/450_1000.jpg'
+    },
+  ]
+
+export const books = [
+    {
+        id: '8',
+        name: 'Libro Feo',
+        price: '$10',
+        description: 'el mejor libro',
+        image: 'https://images.cdn1.buscalibre.com/fit-in/360x360/63/84/6384309f71569bee898862a2125de287.jpg'
+    },
+    {
+        id: '9',
+        name: 'Harry Potter',
+        price: '$10',
+        description: 'Las aventuras de Harry',
+        image: 'https://images.cdn1.buscalibre.com/fit-in/360x360/66/1a/661a3760157941a94cb8db3f5a9d5060.jpg'
+    },
+    {
+        id: '10',
+        name: 'El señor de los anillos',
+        price: '$10',
+        description: 'en la tierra media',
+        image: 'https://images.cdn2.buscalibre.com/fit-in/360x360/e3/bc/e3bcd85377567759874a0664f894a67b.jpg'
+    },
+]
+
+export const pc = [
+    {
+        id: '11',
+        name: 'Pc Gaming',
+        price: '$10000',
+        description: 'corre todo en alto',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6r-vKQVFD2gkB6Iuk7BAnKlqODVArumSu8k6rXoWXaHA0OtCGFskmpkpO29G1gavKtzo&usqp=CAU'
+    }
+]
